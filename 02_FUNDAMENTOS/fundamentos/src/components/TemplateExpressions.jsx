db3587a246d80c672f25e55e4270cf6073113b75
Patrick <patriick.dev@hotmail.com>
2025-07-07 15:21:04 -0300

    feat: Hierarquia de componentes

--- 02_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.jsx
+++ 02_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.jsx
@@ -1,3 +1,4 @@
+import MyComponent from "./MyComponent"
 const TemplateExpressions = () => {
 
     const name = "Patrick"
@@ -10,6 +11,7 @@ const TemplateExpressions = () => {
         <div>
             <h1>Olá {name}, tudo bem?</h1>
             <p>Você atua como: { data.job}</p>
+            <MyComponent/>
         </div>
     )
 }
