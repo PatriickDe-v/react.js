1c7bb3d62dc48e77c0017cf3def0cef1bc7853de
Patrick <patriick.dev@hotmail.com>
2025-07-07 14:35:20 -0300

    feat: importação de componentes

--- 02_FUNDAMENTOS/fundamentos/src/App.js
+++ 02_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+//components
+import FirstComponent from './components/FirstComponent';
+//styles / css
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React!</h1>
+      <FirstComponent/>
     </div>
   );
 }
