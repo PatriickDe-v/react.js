f6cac9428242ee0ae426286d2f75e0bc34c33359
Patrick <patriick.dev@hotmail.com>
2025-07-07 16:17:01 -0300

    feat: Eventos

--- 02_FUNDAMENTOS/fundamentos/src/App.js
+++ 02_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,7 +1,7 @@
 //components
 import FirstComponent from './components/FirstComponent';
 import TemplateExpressions from './components/TemplateExpressions';
-
+import Events from './components/Events';
 //styles / css
 import './App.css';
 
@@ -11,6 +11,7 @@ function App() {
       <h1>Fundamentos React!</h1>
       <FirstComponent/>
       <TemplateExpressions/>
+      <Events/>
     </div>
   );
 }
