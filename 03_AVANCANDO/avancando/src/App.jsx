bd79642cdcdda3e6dfbbc9d3088accee5eb2883a
Patrick <patriick.dev@hotmail.com>
2025-07-09 11:16:07 -0300

    feat: useState na prática

---
+++ 03_AVANCANDO/avancando/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ManageData from '../components/ManageData'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <ManageData/>
+      </div>
+    </>
+  )
+}
+
+export default App
