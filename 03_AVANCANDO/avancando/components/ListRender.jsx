500a7e039a59c8f463e0a4a52ac2928e8046a997
Patrick <patriick.dev@hotmail.com>
2025-07-09 11:56:34 -0300

    feat: propriedade key em listas

--- 03_AVANCANDO/avancando/components/ListRender.jsx
+++ 03_AVANCANDO/avancando/components/ListRender.jsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 const ListRender = () => {
   const [list] = useState(["Matheus", "José", "Maria", "Pedro"]);
 
+  const [users] = useState([
+    { id: 1, nome: "Matheus", age: 34 },
+    { id: 2, nome: "Patrick", age: 28 },
+    { id: 3, nome: "José", age: 42 },
+  ]);
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((user, id) => (
+          <li key={id}>{user}</li>
         ))}
       </ul>
     </div>
