1f52e56ba733cdb3e966963a548da5916ef2531d
Patrick <patriick.dev@hotmail.com>
2025-07-10 11:00:54 -0300

    feat: if e else no JSX

--- 03_AVANCANDO/avancando/components/ConditionalRender.jsx
+++ 03_AVANCANDO/avancando/components/ConditionalRender.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 const ConditionalRender = () => {
   const [x] = useState(false);
+  const [name, setName] = useState("João");
 
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {x && <p>Se X for true sim!</p>}
       {!x && <p>Agora X é falso!</p>}
+      <h1>If ternário</h1>
+      {name === "Patrick" ? (
+        <div>
+          <p>O nome é Patrick</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
+      <button onClick={() => setName("Patrick")}>Clique aqui!</button>
     </div>
   );
 };
