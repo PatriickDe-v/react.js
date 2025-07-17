62eec66066b5e091d820adb181525467e9a285ab
Patrick <patriick.dev@hotmail.com>
2025-07-17 14:22:44 -0300

    feat: links ativos

--- 06_ROUTER/router/src/components/Navbar.jsx
+++ 06_ROUTER/router/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 // 2 - Links com react router
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/About">Sobre</Link>
+      {/*<Link to="/">Home</Link>
+      <Link to="/About">Sobre</Link> */}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/About">Sobre</NavLink>
     </nav>
   );
 };
